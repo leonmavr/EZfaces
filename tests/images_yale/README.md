6387f7af0888a045883dfc63fd7134a8520866b6
0xleo <0xleo.git@gmail.com>
2020-08-01 02:11:28 +0200

    Add basic unit tests

---
+++ tests/images_yale/README.md
@@ -0,0 +1,3 @@
+# About this folder
+
+Credits to Yale dataset, where these images are obtained from http://vision.ucsd.edu/content/yale-face-database.
---
+++ tests/test_face_classifier.py
@@ -0,0 +1,64 @@
+import unittest
+
+import os
+import sys
+this_script_path = os.path.abspath(__file__)
+this_script_folder = os.path.dirname(this_script_path)
+sys.path.insert(1, os.path.join(this_script_folder, '..', 'src'))
+from face_classifier import faceClassifier
+import glob
+import numpy as np
+
+
+class TestUM(unittest.TestCase):
+
+    def test_train_with_olivetti(self):
+        fc = faceClassifier()
+        self.assertEqual(len(fc.data.shape), 2)
+        # data stored as 64*64 row vectors
+        self.assertEqual(fc.data.shape[1], 64*64)
+        # Olivetti data contain 40 subjects
+        self.assertEqual(len(np.unique(fc.target)), 40)
+        fc.train()
+        # their coordinates in eigenface space as a matrix (.W)
+        self.assertEqual(len(fc.W.shape), 2)
+
+
+    def test_train_with_subject(self):
+        fpaths_images = glob.glob(os.path.join(this_script_folder, 'images_yale', 'subject03*png'))
+        fc = faceClassifier()
+        fc.add_img_data(fpaths_images)
+        # data stored as 64*64 row vectors
+        self.assertEqual(fc.data.shape[1], 64*64)
+        # 40 + 1 subjects
+        self.assertEqual(len(np.unique(fc.target)), 41)
+        fc.train()
+        # their coordinates in eigenface space as a matrix (.W)
+        self.assertEqual(len(fc.W.shape), 2)
+
+
+    def test_benchmark(self):
+        fpaths_images = glob.glob(os.path.join(this_script_folder, 'images_yale', 'subject03*png'))
+        if len(fpaths_images) == 0:
+            return
+        fc = faceClassifier(ratio = .725)
+        fc.add_img_data(fpaths_images)
+        fc.benchmark()
+        self.assertNotEqual(fc.classification_report, None)
+        print(fc.classification_report)
+        fc.benchmark(imshow=True, wait_time=0.8, which_labels=[0, 5, 13, 28, 40])
+
+
+    def test_export_import(self):
+        fpaths_images = glob.glob(os.path.join(this_script_folder, 'images_yale', 'subject03*png'))
+        fc = faceClassifier()
+        fc.add_img_data(fpaths_images)
+        # write as pickle files
+        fc.export()
+
+        fc2 = faceClassifier(data_pkl = '/tmp/data.pkl', target_pkl = '/tmp/target.pkl')
+        self.assertEqual(len(np.unique(fc2.target)), 41)
+
+
+if __name__ == '__main__':
+    unittest.main()
