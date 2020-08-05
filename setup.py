c2fc74d8a70840bd5c5b9a514e73353020d52d9f
0xleo <0xleo.git@gmail.com>
2020-08-05 19:44:20 +0300

    Add release link to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='Face recognition using Eigenfaces',
     author='0xLeo',
     author_email='0xleo.git@gmail.com',
-    url='https://github.com/0xLeo/EZfaces',
+    url='https://github.com/0xLeo/EZfaces/archive/alpha.tar.gz',
     packages=['src'],
     include_package_data=True,
     install_requires=['opencv-python',
